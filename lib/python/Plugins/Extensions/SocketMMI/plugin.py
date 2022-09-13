d49f97e746fcd0b9d8357afeb725c10301d44626
atefganm <atef_anim_1974@yahoo.com>
2022-09-14 00:11:11 +0200

    update-25

--- lib/python/Plugins/Extensions/SocketMMI/plugin.py
+++ lib/python/Plugins/Extensions/SocketMMI/plugin.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 from Plugins.Plugin import PluginDescriptor
 from .SocketMMI import SocketMMIMessageHandler
 
@@ -9,7 +10,7 @@ def main(session, **kwargs):
 
 
 def menu(menuid, **kwargs):
-	if menuid == "setup" and socketHandler and socketHandler.connected():
+	if menuid == "cam" and socketHandler and socketHandler.connected():
 		return [(socketHandler.getName(), main, "socket_mmi", 0)]
 	return []
 
